ddba14207ca6161f26c6a099a0ceec2f9a5f8115
LeTruongGiangVTCA0 <gianglt.nde17042@vtc.eud.vn>
2018-06-05 17:34:05 +0700

    test

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Group_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Truong Giang");
         }
     }
 }
